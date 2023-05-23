5cb4d2bc6b8b9747fef908df04bd24ecbfa95dd4
shinykuchi <kuchipudishiny1@gmail.com>
2023-05-23 18:21:31 +0530

    first commit

---
+++ KDrama.java
@@ -0,0 +1,2 @@
+this is a place where everyone gets relaxed,getting destressed,enjoying themselves like in heaven.
+its like a thearapy
---
+++ KPop.java
@@ -0,0 +1,2 @@
+Annyaseyo irabun, this contains kbeauty brands. 
+so can easily purchase on all websites
